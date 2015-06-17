9a2a10505cc0190b8409a295ada19e61f2b890c4
karsten-burger <karsten_burger@gmx.de>
2015-06-17 14:16:50 +0200

    Update csv2libsvm.py
    Now can handle trailing comments; fix a format problem (space between index and value)

--- csv2libsvm.py
+++ csv2libsvm.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
 """
+Based on script from http://fastml.com/
+Repository https://github.com/zygmuntz/phraug/blob/master/csv2libsvm.py.
+
 Convert CSV file to libsvm format. Works only with numeric variables.
 Put -1 as label index (argv[3]) if there are no labels in your file.
 Expecting no headers. If present, headers can be skipped with argv[4] == 1.
+Trailing comments beginning with # are stripped out.
+Example: python csv2libsvm.py data.csv data.libsvm 0 True   # (class label/target is in 1st column, ignore one header line)
+         Reads data.csv and produces data.libsvm.
 """
 
 import sys
@@ -16,9 +22,13 @@ def construct_line( label, line ):
 	new_line.append( label )
 	
 	for i, item in enumerate( line ):
+                item = item.strip()
+	        if "#" in item:
+	          item = item.split('#', 1)[0].strip()   # folgenden Kommentar weglassen
+
 		if item == '' or float( item ) == 0.0:
 			continue
-		new_item = "%s:%s" % ( i + 1, item )
+		new_item = "%d:%s" % ( i + 1, item )
 		new_line.append( new_item )
 	new_line = " ".join( new_line )
 	new_line += "\n"
@@ -43,14 +53,25 @@ i = open( input_file )
 o = open( output_file, 'wb' )
 
 reader = csv.reader( i )
+ 
 if skip_headers:
+	print "skip header line"
 	headers = reader.next()
 
 for line in reader:
+	# ignore comment lines
+	if len(line) > 0: 
+	    item = line[0].strip()
+	    if item[0] == "#":
+	        print "ignoring: ", line
+	        continue
+	       
 	if label_index == -1:
 		label = 1
 	else:
 		label = line.pop( label_index )
 		
+	# remove trailing comments:
+        #print line
 	new_line = construct_line( label, line )
 	o.write( new_line )
